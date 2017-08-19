2938c67b4d4238a217c9e087cfd39d4dd7fff3d7
Pedro Saratscheff <pedro@saratscheff.cl>
2017-08-19 18:37:26 -0300

    basic wallet code

--- controllers/etherscanCtrl.js
+++ controllers/etherscanCtrl.js
@@ -1,6 +1,5 @@
 // controllers/etherscan.js
-
-const api_key = process.env.etherscan_api_key; // https://etherscan.io/myapikey
+const ETHERSCAN_API_KEY = process.env.etherscan_api_key; // https://etherscan.io/myapikey
 
 module.exports = {
     api_key: api_key
---
+++ obj/wallet.js
@@ -0,0 +1,33 @@
+// obj/wallet.js
+const ETHERSCAN_API_KEY = process.env.etherscan_api_key; // https://etherscan.io/myapikey
+
+// API Documentation => https://etherscan.io/apis
+
+// Class instantiation
+var wa = Wallet.prototype;
+
+// Constructor
+function Wallet(name, dir) {
+    this.name = name;
+    this.dir = dir;
+}
+
+// Methods
+var request = require('request');
+
+wa.getBalance = function(callback) {
+  var url = 'https://api.etherscan.io/api?module=account&action=balance&address=' + this.dir + '&tag=latest&apikey=' + ETHERSCAN_API_KEY;
+  console.log(url);
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      body = JSON.parse(body);
+      callback((parseFloat(body['result'])/1000000000000000000).toFixed(18));
+    } else if (error){
+      callback("ERROR on GetBalance: " + error);
+    } else {
+      callback("ERROR on GetBalance: Status code => " + response.statusCode);
+    }
+  });
+};
+
+module.exports = Wallet;
