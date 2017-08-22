695f43ac8dcb9a945788c4ec4739e21076053437
Pedro Saratscheff <pedro@saratscheff.cl>
2017-08-22 18:27:48 -0300

    markdown for help message

--- controllers/telegramCtrl.js
+++ controllers/telegramCtrl.js
@@ -65,8 +65,10 @@ function process_message(user, message) {
     // ================================Help=============================
     if(message.text.toLowerCase().indexOf("/help") === 0 || message.text.toLowerCase().indexOf("/start") === 0 || message.text.toLowerCase().indexOf("/about") === 0) {
       var answer =
-      "Hi!\nType a forward slash ( / ) to see the list of available commands.\nCancel a command waiting for an answer by typing *cancel* (such as /set\_arbitrage\_minimum).\nYou should receive a 'Sure' confirmation meaning the command was cancelled.";
-      telegram.sendMessage(message.chat.id, answer);
+      "Hi!\nType a forward slash ( / ) to see the list of available commands.\nCancel a command waiting for an answer by typing *cancel*.\nYou should receive a 'Sure' confirmation meaning the command was cancelled.";
+      telegram.sendMessage(message.chat.id, answer, {
+        parse_mode: "Markdown"
+      });
     // ================================Ether Status=============================
     } else if(message.text.toLowerCase().indexOf("/eth") === 0 || message.text.toLowerCase().indexOf("/ether") === 0) {
       var tries = 3;
