3bc1f3e7da462eb686cdd9495eb2894cabefe5f0
Pedro Saratscheff <pedro@saratscheff.cl>
2017-08-22 17:55:33 -0300

    update to new ethermine.org API

--- controllers/ethermineCtrl.js
+++ controllers/ethermineCtrl.js
@@ -3,30 +3,39 @@
 function miner_status_telegram(address, callback) {
   var request = require('request');
 
-  request('https://ethermine.org/api/miner_new/' + address, function (error, response, body) {
+  request('https://api.ethermine.org/miner/' + address + '/workers', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       body = JSON.parse(body);
-      var workers = body['workers'];
+      var workers = body['data'];
+      request('https://api.ethermine.org/miner/' + address + '/currentStats', function (error, response, body2) {
+        if (!error && response.statusCode == 200) {
+          body2 = JSON.parse(body2);
+          var stats = body2['data'];
 
-      var answer = '*Miners Status*';
-      var workers_count = 0;
-      for (var worker in workers){
-        workers_count += 1;
-        if (workers.hasOwnProperty(worker)){
-          answer += "\n--- " + worker + "---";
-          answer += "\nReported hashrate: " + body['workers'][worker]['reportedHashRate'];
-          answer += "\nValid shares: " + body['workers'][worker]['validShares'];
-          answer += "\nCurrent hashrate: " + body['workers'][worker]['hashrate'];
+          var answer = '*Miners Status*';
+          var workers_count = 0;
+          for (var worker in workers){
+            workers_count += 1;
+            answer += "\n--- " + workers[worker]['worker'] + "---";
+            answer += "\nReported hashrate: " + (workers[worker]['reportedHashrate']/1000000).toFixed(2) + 'MH/s';
+            answer += "\nValid shares: " + workers[worker]['validShares'];
+            answer += "\nCurrent hashrate: " + (workers[worker]['currentHashrate']/1000000).toFixed(2) + 'MH/s';
+          }
+          eth_to_weis = 1000000000000000000; // 1 Ether in Weis
+          answer += "\n*Payments*";
+          answer += "\nUnpaid amount: " + (stats['unpaid']/eth_to_weis);
+          answer += "\nDays until paid 1ETH: " + ((1-(stats['unpaid']/eth_to_weis))/(24*60*stats['coinsPerMin']));
+          answer += "\n*Earnings per*";
+          answer += "\nDay: " + (24*60*stats['coinsPerMin']);
+          answer += "\nWeek: " + (7*24*60*stats['coinsPerMin']);
+          answer += "\nMonth: " + (30*24*60*stats['coinsPerMin']);
+          callback(false, answer, workers_count);
+        } else if (error){
+          callback(error, null, null);
+        } else {
+          callback('ERROR: Status code: ' + response.statusCode, null, null);
         }
-      }
-      answer += "\n*Payments*";
-      answer += "\nUnpaid amount: " + (body['unpaid']/body['settings']['minPayout']);
-      answer += "\nDays until paid 1ETH: " + ((1-(body['unpaid']/body['settings']['minPayout']))/(24*60*body['ethPerMin']));
-      answer += "\n*Earnings per*";
-      answer += "\nDay: " + (24*60*body['ethPerMin']);
-      answer += "\nWeek: " + (7*24*60*body['ethPerMin']);
-      answer += "\nMonth: " + (30*24*60*body['ethPerMin']);
-      callback(false, answer, workers_count);
+      });
     } else if (error){
       callback(error, null, null);
     } else {
@@ -39,22 +48,20 @@ function check_miner_ok(user, callback) {
   var request = require('request');
   var address = user.miner_address;
 
-  request('https://ethermine.org/api/miner_new/' + address, function (error, response, body) {
+  request('https://api.ethermine.org/miner/' + address + '/workers', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       body = JSON.parse(body);
-      var workers = body['workers'];
+      var workers = body['data'];
 
       var answer = '*WARNING!!*';
       var hashing_0 = false;
       var workers_count = 0;
       for (var worker in workers){
         workers_count += 1;
-        if (workers.hasOwnProperty(worker)){
-          if (body['workers'][worker]['reportedHashRate'] == "0H/s") {
-            answer += "\n" + worker + " is reporting 0H/s";
-            hashing_0 = true;
-          };
-        }
+        if (workers[worker]['reportedHashrate'] == 0) {
+          answer += "\n" + workers[worker]['worker'] + " is reporting 0H/s";
+          hashing_0 = true;
+        };
       }
       callback(false, answer, workers_count, hashing_0, user);
     } else if (error){
